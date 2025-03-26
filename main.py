4170b64008964c63ab8cf3caa87b3feee6c53154
Sahil Patra <38413143+iamonjarvis@users.noreply.github.com>
2025-03-26 23:54:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -230,4 +230,6 @@ async def upload_file(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
